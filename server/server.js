54be072500bf038f9f800dc3c90eaa7f2608679b
ecodingdeveloper <echdevs@ecodinghub.com>
2025-04-13 12:13:18 +0530

    some updates

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
+require('dotenv').config();
 const multer = require('multer');
 const fs = require('fs');
 const Candidate = require('./models/Candidate');
@@ -12,7 +13,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-mongoose.connect('mongodb://localhost:27017/hrms', {
+mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
