08316a3056db4359ac7d438b7801ef84584c3192
Dinesh Kumar <162158359+diineshkumarr@users.noreply.github.com>
2025-04-13 12:54:12 +0530

    Update Login.jsx

--- register-page/src/components/Login.jsx
+++ register-page/src/components/Login.jsx
@@ -17,7 +17,7 @@ const Login = () => {
     e.preventDefault();
 
     try {
-      const res = await fetch('http://localhost:5000/api/auth/login', {
+      const res = await fetch('https://assignment-2-frontend.onrender.com/api/auth/login', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify(formData),
